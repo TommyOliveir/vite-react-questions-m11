a581cdbd16b4270e0b0896d90079bb61818e6343
TommyOliveir <laridatommy@gmail.com>
2022-12-05 15:04:48 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Question from './Question'
+import { nanoid } from 'nanoid'
 
 
 function App() {
   const [questions, setQuestions] = useState([])
+  const [showQuiz, setShowQuiz] = useState(false)
+  const [currentQuestions, setCurrentQuestions] = useState([{
+    id: nanoid(),
+    question: "",
+    type: "",
+    correct_answer: "",
+    incorrect_answers: []
+
+  }])
 
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=5')
@@ -17,16 +27,46 @@ function App() {
   console.log("tom", questions)
   console.log("hey", questions[1])
 
-  const questionElements = questions.map((item, index) => {
-    return <Question key={item.question} question={item.question} number={index + 1} type={item.type} />
+
+
+  function showQuizBtn() {
+    setShowQuiz(prevShowQuiz => prevShowQuiz = true)
+    setCurrentQuestions(questions.map(question => {
+      return {
+        question: question.question,
+        type: question.type,
+        correct_answer: question.correct_answer,
+        incorrect_answers: question.incorrect_answers
+      }
+    }))
+  }
+  console.log("current question", currentQuestions)
+
+  ///map original question from api
+  // const questionElements = questions.map((item, index) => {
+  //    return <Question  question={item.question} number={index + 1} type={item.type} incorrect_answers={item.incorrect_answers }
+  //   />
+  // }
+  // )
+  const questionElements = currentQuestions.map((item, index) => {
+     return <Question  key={item.id} question={item.question} number={index + 1} type={item.type} incorrect_answers={item.incorrect_answers }
+    />
   }
   )
+
+
+
 
   return (
     <div className="App">
       <main>
-        <h1>Quiz bee</h1>
-        {questionElements}
+        {!showQuiz && <h1>Quiz bee</h1>}
+        {!showQuiz && <p>Enjoy your quiz</p>}
+        {showQuiz && questionElements}
+
+    
+        <button onClick={showQuizBtn}>Start Quiz</button>
+        {currentQuestions.question}
       </main>
 
 
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 function Question(props) {
+ 
     const styles = {
         backgroundColor: props.type === "multiple" ? "#59E391" : props.type === "boolean" ?  "yellow" : "white"
     }
-    
+
+const incorrectanswers = props.incorrect_answers.map(answer => <p className='answer'>{answer}</p>)
   return (
     <div>
       <p style={styles}>{props.number}. {props.question}</p>
+      { props.type === "boolean" ? <div><p>true</p><p>false</p></div>  : <p>Multiple choice {incorrectanswers}</p> }
     </div>
   )
 }
