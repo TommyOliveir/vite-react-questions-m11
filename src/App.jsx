f35a027380bde2d8ffa09e807bdce13ebfd89c37
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2022-12-05 18:27:25 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,34 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Question from './Question'
-import { nanoid } from 'nanoid'
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import Question from "./Question";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [questions, setQuestions] = useState([])
-  const [showQuiz, setShowQuiz] = useState(false)
-  const [currentQuestions, setCurrentQuestions] = useState([{
-    id: nanoid(),
-    question: "",
-    type: "",
-    correct_answer: "",
-    incorrect_answers: [],
-   
-    // user_answered: "", 
-    // user_scored: "", 
+  const [questions, setQuestions] = useState([]);
+  const [showQuiz, setShowQuiz] = useState(false);
+  const [currentQuestions, setCurrentQuestions] = useState([
+    {
+      id: nanoid(),
+      question: "",
+      type: "",
+      correct_answer: "",
+      incorrect_answers: [],
 
-  }])
+      // user_answered: "",
+      // user_scored: "",
+    },
+  ]);
 
   useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=5')
+    fetch("https://opentdb.com/api.php?amount=5")
       .then((response) => response.json())
       .then((data) => setQuestions(data.results));
+  }, []);
 
-  }, [])
+  console.log("tom", questions);
+  console.log("hey", questions[1]);
 
-
-  console.log("tom", questions)
-  console.log("hey", questions[1])
-
-
-//working
+  //working
   // function showQuizBtn() {
   //   setShowQuiz(prevShowQuiz => prevShowQuiz = true)
   //   setCurrentQuestions(questions.map(question => {
@@ -45,21 +42,23 @@ function App() {
   // }
 
   function showQuizBtn() {
-    setShowQuiz(prevShowQuiz => prevShowQuiz = true)
-    setCurrentQuestions(prevcurrentQuestions => prevcurrentQuestions = questions.map(item => {
-      console.log("quesion map",  item)
-      return {
-        
-        question: item.question,
-        type: item.type,
-        correct_answer: item.correct_answer,
-        incorrect_answers: item.incorrect_answers,
-        multiple_choices: ["a", "b"]
-      }
-    }))
+    setShowQuiz((prevShowQuiz) => (prevShowQuiz = true));
+    setCurrentQuestions(
+      (prevcurrentQuestions) =>
+        (prevcurrentQuestions = questions.map((item) => {
+          console.log("quesion map", item);
+          return {
+            question: item.question,
+            type: item.type,
+            correct_answer: item.correct_answer,
+            incorrect_answers: item.incorrect_answers,
+            multiple_choices: [...item.incorrect_answers, item.correct_answer],
+          };
+        }))
+    );
   }
-  console.log("current question", currentQuestions)
-  console.log("multi", currentQuestions[0].multiple_choices)
+  console.log("current question", currentQuestions);
+  console.log("multi", currentQuestions[0].multiple_choices);
 
   ///map original question from api
   // const questionElements = questions.map((item, index) => {
@@ -68,14 +67,18 @@ function App() {
   // }
   // )
   const questionElements = currentQuestions.map((item, index) => {
-    return <Question key={item.id} question={item.question} number={index + 1} type={item.type}  correct_answer={item.correct_answer}
-    incorrect_answers={item.incorrect_answers} 
-    />
-  }
-  )
-
-
-
+    return (
+      <Question
+        key={item.id}
+        question={item.question}
+        number={index + 1}
+        type={item.type}
+        correct_answer={item.correct_answer}
+        incorrect_answers={item.incorrect_answers}
+        choices={item.multiple_choices}
+      />
+    );
+  });
 
   return (
     <div className="App">
@@ -84,14 +87,11 @@ function App() {
         {!showQuiz && <p>Enjoy your quiz</p>}
         {showQuiz && questionElements}
 
-
         <button onClick={showQuizBtn}>Start Quiz</button>
         {currentQuestions.question}
       </main>
-
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React from "react";
 
 function Question(props) {
- 
-    const styles = {
-        backgroundColor: props.type === "multiple" ? "#59E391" : props.type === "boolean" ?  "yellow" : "white"
-    }
+  const styles = {
+    backgroundColor:
+      props.type === "multiple"
+        ? "#59E391"
+        : props.type === "boolean"
+        ? "yellow"
+        : "white",
+  };
 
-const incorrectanswers = props.incorrect_answers.map(answer => <p className='answer'>{answer}</p>)
-const correctanswer = props.correct_answer
+  const incorrectanswers = props.incorrect_answers.map((answer) => (
+    <p className="answer">{answer}</p>
+  ));
+  const choices = props.choices.map((answer) => (
+    <p className="answer">{answer}</p>
+  ));
+  const correctanswer = props.correct_answer;
 
   return (
     <div>
-      <p style={styles}>{props.number}. {props.question}</p>
-      { props.type === "boolean" ? <div><p>{incorrectanswers}</p><p className='correct-answer'>{correctanswer}</p></div> 
-       : <div className='multiple choice'>Multiple choice {incorrectanswers} <p className='correct-answer'>{correctanswer}</p> </div> }
+      <p style={styles}>
+        {props.number}. {props.question}
+      </p>
+      {props.type === "boolean" ? (
+        <div>
+          <p>{incorrectanswers}</p>
+          <p className="correct-answer">{correctanswer}</p>
+        </div>
+      ) : (
+        <div className="multiple choice">
+          Multiple choice
+          <p className="correct-answer">{correctanswer}</p> {choices}{" "}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Question
+export default Question;
